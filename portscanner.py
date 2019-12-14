bbd3bf223a8642d47a3c69155bb1ecf9fd6408fb
Matt <38298036+worpel@users.noreply.github.com>
2019-12-14 16:02:05 +0000

    Refactored port range validation and other minor tweaks

--- portscanner.py
+++ portscanner.py
@@ -2,10 +2,10 @@
 
 # Import socket module for port scanning functionality
 import socket
-# Import module for regular expressions
-import re
 # Import datetime class for timestamps
 from datetime import datetime
+# Import scapy for packet manipulation
+# from scapy.all import *
 
 # Prompt user for hostname. If it is a domain, resolve its IP address
 host = input('Enter the hostname or IP address of the target: ')
@@ -21,19 +21,23 @@ while True:
         break
 
 # Define port range. If empty, revert to 1-1023 as default (these ports can only be opened by privileged users)
-portRange = input('Define port range that you want to scan (leave blank for well-known ports 1-1023): ')
+portRange = input(
+    'Define port range that you want to scan (leave blank for well-known ports 1-1023): ')
 portRange = portRange if portRange else '1-1023'
 
-# Ensure the given ports fall between 1-65535 (does not compare each value in the range)
-if re.match(r'^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])[-]([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$', portRange):
-    portRange = portRange
-else:
-    portRange = input('Please enter a valid port range')
+while True:
+    # Format the port range for passing through to socket
+    portRange = portRange.split('-')
+    startPort = int(portRange[0])
+    endPort = int(portRange[1])
 
-# Format the port range for passing through to socket
-portRange = portRange.split('-')
-startPort = int(portRange[0])
-endPort = int(portRange[1])
+    # Check port range is valid
+    if startPort >= 0 and startPort < endPort:
+        if endPort > startPort and endPort <= 65535:
+            portRange = portRange
+            break
+    else:
+        portRange = input('Please enter a valid port range: ')
 
 # Output scan start date/time
 print('Started scan on', datetime.now().strftime(f'%d-%m-%Y at %H:%M:%S'))
@@ -51,7 +55,7 @@ for port in range(startPort, endPort + 1):
         # Open socket TCP connection
         connection = s.connect_ex((host, port))
         # If connection successful, return open port
-        if connection ==  0:
+        if connection == 0:
             print(datetime.now().strftime('%H:%M:%S') + f': Port {port} is open')
     # Handle socket exceptions
     except socket.error:
